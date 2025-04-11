534d742dde93808b9944464677139297fadce1bf
Devlin <devlincodes@gmail.com>
2025-04-10 22:36:25 -0700

    add initial player animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,17 +1,23 @@
 extends CharacterBody2D
 
-const SPEED = 4
+const SPEED = 2
 var direction := Vector2(SPEED, 0)
 
-func _physics_process(delta: float) -> void:
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _physics_process(_delta: float) -> void:
 	if Input.is_action_pressed("up"):
 		direction = Vector2(0, -SPEED)
+		animation_player.assigned_animation = "Up"
 	if Input.is_action_pressed("down"):
 		direction = Vector2(0, SPEED)
+		animation_player.assigned_animation = "Down"
 	if Input.is_action_pressed("left"):
 		direction = Vector2(-SPEED, 0)
+		animation_player.assigned_animation = "Left"
 	if Input.is_action_pressed("right"):
 		direction = Vector2(SPEED, 0)
+		animation_player.assigned_animation = "Right"
 
 	position += direction
 
