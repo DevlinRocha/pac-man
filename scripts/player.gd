4d8e63f298889ee244490921d865f24af32dab9d
Devlin <devlin@devlinrocha.com>
2025-04-12 02:58:20 -0700

    prevent player from moving directly into walls

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,20 +4,30 @@ const SPEED = 2
 var direction := Vector2(SPEED, 0)
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var ray_cast_up: RayCast2D = $RayCastUp
+@onready var ray_cast_down: RayCast2D = $RayCastDown
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_pressed("up"):
-		direction = Vector2(0, -SPEED)
-		animation_player.assigned_animation = "Up"
+		if not ray_cast_up.is_colliding():
+			direction = Vector2(0, -SPEED)
+			animation_player.assigned_animation = "Up"
+
 	if Input.is_action_pressed("down"):
-		direction = Vector2(0, SPEED)
-		animation_player.assigned_animation = "Down"
+		if not ray_cast_down.is_colliding():
+			direction = Vector2(0, SPEED)
+			animation_player.assigned_animation = "Down"
 	if Input.is_action_pressed("left"):
-		direction = Vector2(-SPEED, 0)
-		animation_player.assigned_animation = "Left"
+		if not ray_cast_left.is_colliding():
+			direction = Vector2(-SPEED, 0)
+			animation_player.assigned_animation = "Left"
 	if Input.is_action_pressed("right"):
-		direction = Vector2(SPEED, 0)
-		animation_player.assigned_animation = "Right"
+		if not ray_cast_right.is_colliding():
+			direction = Vector2(SPEED, 0)
+			animation_player.assigned_animation = "Right"
 
 	position += direction
 
