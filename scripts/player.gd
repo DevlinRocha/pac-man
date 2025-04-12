bf7a035769d2106a1e82a596ec63267183ddee1a
Devlin <devlin@devlinrocha.com>
2025-04-12 13:15:25 -0700

    add 3 ray casts in each direction

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,31 +3,37 @@ extends CharacterBody2D
 const SPEED = 2
 var direction := Vector2(SPEED, 0)
 
+
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var ray_cast_up: RayCast2D = $RayCastUp
-@onready var ray_cast_down: RayCast2D = $RayCastDown
-@onready var ray_cast_left: RayCast2D = $RayCastLeft
-@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var ray_cast_group_up: Node2D = $RayCastGroupUp
+@onready var ray_cast_group_down: Node2D = $RayCastGroupDown
+@onready var ray_cast_group_left: Node2D = $RayCastGroupLeft
+@onready var ray_cast_group_right: Node2D = $RayCastGroupRight
+
+
+func can_move(ray_group: Node2D) -> bool:
+	for ray in ray_group.get_children():
+		if ray is RayCast2D and ray.is_colliding():
+			return false
+	return true
 
 
 func _physics_process(_delta: float) -> void:
-	if Input.is_action_pressed("up"):
-		if not ray_cast_up.is_colliding():
-			direction = Vector2(0, -SPEED)
-			animation_player.assigned_animation = "Up"
+	if Input.is_action_pressed("up") and can_move(ray_cast_group_up):
+		direction = Vector2(0, -SPEED)
+		animation_player.assigned_animation = "Up"
 
-	if Input.is_action_pressed("down"):
-		if not ray_cast_down.is_colliding():
-			direction = Vector2(0, SPEED)
-			animation_player.assigned_animation = "Down"
-	if Input.is_action_pressed("left"):
-		if not ray_cast_left.is_colliding():
-			direction = Vector2(-SPEED, 0)
-			animation_player.assigned_animation = "Left"
-	if Input.is_action_pressed("right"):
-		if not ray_cast_right.is_colliding():
-			direction = Vector2(SPEED, 0)
-			animation_player.assigned_animation = "Right"
+	if Input.is_action_pressed("down") and can_move(ray_cast_group_down):
+		direction = Vector2(0, SPEED)
+		animation_player.assigned_animation = "Down"
+
+	if Input.is_action_pressed("left") and can_move(ray_cast_group_left):
+		direction = Vector2(-SPEED, 0)
+		animation_player.assigned_animation = "Left"
+
+	if Input.is_action_pressed("right") and can_move(ray_cast_group_right):
+		direction = Vector2(SPEED, 0)
+		animation_player.assigned_animation = "Right"
 
 	position += direction
 
