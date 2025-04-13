ec5442d16a3c405c16dba8e063e7f79d120d2cd2
Devlin <devlin@devlinrocha.com>
2025-04-12 20:08:19 -0700

    remove print statements

--- scripts/play_area.gd
+++ scripts/play_area.gd
@@ -5,8 +5,6 @@ func _ready() -> void:
 	body_exited.connect(_on_body_exited)
 
 func _on_body_exited(body: Node2D) -> void:
-	print(body)
-	print(body.position.x)
 	if body.position.x > 800:
 		body.position.x = 340.0
 	elif body.position.x > 341:
