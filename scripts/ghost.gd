049ffc5d3a1046c4a96f5aa941f28cd300411f48
Devlin <devlin@devlinrocha.com>
2025-04-13 04:06:01 -0700

    refactor logic into `change_color` function

--- scripts/ghost.gd
+++ scripts/ghost.gd
@@ -2,12 +2,20 @@ extends CharacterBody2D
 
 enum GhostType {BLINKY, PINKY, INKY, CLYDE}
 
-@onready var sprite_2d: Sprite2D = $Sprite2D
-
 @export var ghost_identity: GhostType
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 
 func _ready() -> void:
+	change_color()
+
+
+func _physics_process(_delta: float) -> void:
+	move_and_slide()
+
+
+func change_color() -> void:
 	var material: ShaderMaterial = sprite_2d.material
 	var ghost_color: Color
 
@@ -20,5 +28,5 @@ func _ready() -> void:
 			ghost_color = Color.CYAN
 		GhostType.CLYDE:
 			ghost_color = Color.ORANGE
-	
+
 	material.set_shader_parameter("new_color", ghost_color)
