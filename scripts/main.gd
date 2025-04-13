8e64f075a13f4b1b48d8f6731e161d072e26b524
Devlin <devlin@devlinrocha.com>
2025-04-12 23:05:34 -0700

    power pellets gain 50 points

--- scripts/main.gd
+++ scripts/main.gd
@@ -6,6 +6,10 @@ extends Node
 
 func _ready() -> void:
 	maze.pellet_collected.connect(_on_pellet_collected)
+	maze.power_pellet_collected.connect(_on_power_pellet_collected)
 
 func _on_pellet_collected() -> void:
 	ui.increase_score(10)
+
+func _on_power_pellet_collected() -> void:
+	ui.increase_score(50)
--- scripts/maze.gd
+++ scripts/maze.gd
@@ -1,16 +1,23 @@
 extends TileMapLayer
 
-@export var pellet_scene: PackedScene
+const PELLET = preload("res://scenes/pellet.tscn")
+const POWER_PELLET = preload("res://scenes/power_pellet.tscn")
 
 signal pellet_collected
+signal power_pellet_collected
 
 func _ready():
 	var pellet_positions = get_used_cells_by_id(1)
+	var power_pellet_positions = get_used_cells_by_id(2)
 
-	switch_tile_to_scene(1, pellet_scene, _on_pellet_collected)
+	switch_tile_to_scene(1, PELLET, _on_pellet_collected)
+	switch_tile_to_scene(2, POWER_PELLET, _on_power_pellet_collected)
 
 func _on_pellet_collected():
 	pellet_collected.emit()
+
+func _on_power_pellet_collected():
+	power_pellet_collected.emit()
 
 func switch_tile_to_scene(source_id: int, scene: PackedScene, collectedHandler: Callable) -> void:
 	var tile_positions = get_used_cells_by_id(source_id)
