0c1e99180b860f4f6e0d1c215c6f52d385c8251c
Devlin <devlin@devlinrocha.com>
2025-04-14 16:48:08 -0700

    refactor logic

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
-const SPEED = 2
-var direction := Vector2(SPEED, 0)
+enum Direction {UP, DOWN, LEFT, RIGHT}
 
+const SPEED = 2
+
+var current_direction := Vector2.RIGHT * SPEED
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var ray_cast_group_up: Node2D = $RayCastGroupUp
@@ -11,11 +13,9 @@ var direction := Vector2(SPEED, 0)
 @onready var ray_cast_group_right: Node2D = $RayCastGroupRight
 
 
-enum Direction {UP, DOWN, LEFT, RIGHT}
-
-func can_move(direction: Direction) -> bool:
+func can_move(new_direction: Direction) -> bool:
 	var ray_group: Node2D
-	match direction:
+	match new_direction:
 		Direction.UP:
 			ray_group = ray_cast_group_up
 		Direction.DOWN:
@@ -33,21 +33,21 @@ func can_move(direction: Direction) -> bool:
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_pressed("up") and can_move(Direction.UP):
-		direction = Vector2(0, -SPEED)
+		current_direction = Vector2.UP * SPEED
 		animation_player.assigned_animation = "Up"
 
 	if Input.is_action_pressed("down") and can_move(Direction.DOWN):
-		direction = Vector2(0, SPEED)
+		current_direction = Vector2.DOWN * SPEED
 		animation_player.assigned_animation = "Down"
 
 	if Input.is_action_pressed("left") and can_move(Direction.LEFT):
-		direction = Vector2(-SPEED, 0)
+		current_direction = Vector2.LEFT * SPEED
 		animation_player.assigned_animation = "Left"
 
 	if Input.is_action_pressed("right") and can_move(Direction.RIGHT):
-		direction = Vector2(SPEED, 0)
+		current_direction = Vector2.RIGHT * SPEED
 		animation_player.assigned_animation = "Right"
 
-	position += direction
+	position += current_direction
 
 	move_and_slide()
