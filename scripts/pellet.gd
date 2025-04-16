868bddfa418974ee7c23c846cb75e75ba8a56e93
Devlin <devlin@devlinrocha.com>
2025-04-16 08:06:34 -0700

    only player can collect pellets

--- scripts/pellet.gd
+++ scripts/pellet.gd
@@ -6,5 +6,6 @@ func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body: Node2D) -> void:
-	queue_free()
-	collected.emit()
+	if body.is_in_group("player"):
+		queue_free()
+		collected.emit()
